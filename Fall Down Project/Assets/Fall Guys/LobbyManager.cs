8b3ed54665397774f78b027ef74c81308695c462
Prasad_Chandole <73127970+PrasadPDC@users.noreply.github.com>
2021-03-28 12:09:16 +0530

    chai pilo
    chai pilo

---
+++ Fall Down Project/Assets/Fall Guys/LobbyManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UserNameMenu;
+    [SerializeField] private GameObject connetPanel;
+
+    [SerializeField] private TMP_InputField UserNameInputField;
+    [SerializeField] private TMP_InputField JoinGameInputField;
+    [SerializeField] private TMP_InputField CreateGameInputField;
+
+    [SerializeField] private GameObject StartButton;
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersionName);
+    }
+    private void Start()
+    {
+        UserNameMenu.SetActive(true);
+    }
+    public void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("isconneted");
+    }
+
+    public void UserNameInput()
+    {
+        if (UserNameInputField.text.Length >= 4)
+        {
+            StartButton.SetActive(true);
+        }
+        else
+            StartButton.SetActive(false);
+    }
+
+    public void startButtonClick()
+    {
+        UserNameMenu.SetActive(false);
+        PhotonNetwork.playerName = UserNameInputField.text;
+    }
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(CreateGameInputField.text, new RoomOptions() { maxPlayers = 5 }, null);
+    }
+
+    public void JoinRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.maxPlayers = 5;
+        PhotonNetwork.JoinOrCreateRoom(JoinGameInputField.text, roomOptions, TypedLobby.Default);
+    }
+    public void OnJointRoom()
+    {
+        PhotonNetwork.LoadLevel("Level");
+    }
+}
